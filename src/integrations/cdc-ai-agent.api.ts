ac140ddbb4ca7dfa0e168ba398e94b914e1631da
Ricardo Arcifa <ricardo.arcifa@cronoslabs.org>
2024-09-24 08:43:56 +0200

    fix api endpoint

--- src/integrations/cdc-ai-agent.api.ts
+++ src/integrations/cdc-ai-agent.api.ts
@@ -8,7 +8,7 @@ import { CdcAiAgentResponse, Method } from './cdc-ai-agent.interfaces';
  * @returns {Promise<CdcAiAgentResponse>} - The response from the CDC AI Agent service.
  */
 export const generateQuery = async (query: string, options: object): Promise<CdcAiAgentResponse> => {
-  const url = 'http://localhost:8000/api/v1/cdc-ai-agent-service/query';
+  const url = 'https://ai-agent-api.crypto.com/api/v1/cdc-ai-agent-service/query';
   const payload = { query, options };
 
   try {
